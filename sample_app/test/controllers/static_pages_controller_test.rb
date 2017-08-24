1b06c74c9c4c1e1fc40b02f4e46ef506967449fe
Mitzi Cubedo <mitzicubedo@gmail.com>
2017-08-24 11:27:24 -0700

    Finish static pages :disappointed_relieved:

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -5,6 +5,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     @base_title = "Ruby on Rails Tutorial Sample App"
   end
 
+  test "should get root" do
+    get "/"
+    assert_response :success
+  end
+
   test "should get help" do
     get static_pages_help_url
     assert_response :success
@@ -21,5 +26,11 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     get static_pages_about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
